32bafe6380ee6e29aa5b160d151b38b9248bdf73
Ashita J <jagasia.a@northeastern.edu>
2019-09-23 18:29:42 -0700

    Environment Variables

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response, request, jsonify
-import mysql.connector as mariadb
+#import mysql.connector as mariadb
 from config.loggingfilter import *
 from config.envvar import *
 
@@ -25,3 +25,22 @@ def health() -> Response:
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
+
+
+
+#===================================================================
+# API ENDPOINTS
+#===================================================================
+
+@app.route('getacc', methods=['POST'])
+def getacc():
+    res = cur.execute("Select id, first_name, last_name, email from user_info")
+    for row in res:
+        items = {}
+        items['id'] = str(row[0])
+        items['first_name'] = str(row[1])
+        items['last name'] = str(row[2])
+        items['email'] = str(row[3])
+        return Response(json.dumps(status), status=200, mimetype='application/json')
+
+
