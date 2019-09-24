4745b7a037c3f1b243e00a329c3509ff75db2c56
Ashita J <jagasia.a@northeastern.edu>
2019-09-24 14:30:04 -0700

    API Endpoints

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, Response, request, jsonify
-#import mysql.connector as mariadb
+import mysql.connector as mariadb
+from datetime import datetime
 from config.loggingfilter import *
 from config.envvar import *
 
@@ -24,8 +25,6 @@ def health() -> Response:
     status["ok"] = True
     return Response(json.dumps(status), status=200, mimetype='application/json')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
 
 
 
@@ -33,68 +32,111 @@ if __name__ == '__main__':
 # API ENDPOINTS
 #===================================================================
 
-@app.route('getacc', methods=['POST'])
-def getacc():
-    cur = get_db().cursor()
-    res = cur.execute("Select id, first_name, last_name, email, password, account_created, account_updated from user_info")
-    for row in res:
-        items = {}
-        items['id'] = str(row[0])
-        items['first_name'] = str(row[1])
-        items['last name'] = str(row[2])
-        items['email'] = str(row[3])
-        items['password'] = str(row[4])
-        items['account_created'] = str(row[5])
-        items['account_updated'] = str(row[5])
-
-        return Response(json.dumps(status), status=200, mimetype='application/json')
-    return Response(status=400)
-
-
-@app.route("/CreateAccount", methods =['POST'])
-def CreateAccount():
-    try:
+@app.route('/v1/user', methods=['GET','POST', 'PUT'])
+def v1/user():
+    if request.method == 'POST':
+        #CREATE NEW USER
+        try:
         first_name = request.json['first_name']
         last_name = request.json['last_name']
         email = request.json['email']
         password = request.json['password']
+        account_created = datetime.now()
+        account_updated = datetime.now()
         cur = get_db().cursor()
-        checkUsername = cur.execute("Select user_id from user_info where email=? Limit 1", (email))
+        checkUsername = cur.execute("Select email from user_info where email=? Limit 1", (email))
         if checkUsername.fetchall():
             response={}
             response["error"] = "Username already exists !!!!!"
             return Response(json.dumps(response), status=400, mimetype='application/json')
 
 
-        res = cur.execute("INSERT into user_info (first_name, last_name, email, password) values(?,?,?,?,?);", (first_name, last_name, email, password))
+        res = cur.execute("INSERT into user_info (first_name, last_name, email, password, account_created, account_updated) values(?,?,?,?,?,?,?);", (first_name, last_name, email, password, account_created, account_updated))
         get_db().commit()
-        return Response(status=200)
+        return Response(status=201)
     except Exception as e:
         get_db().rollback()
         print(e)
-        return Response(status=403)
-
-
-@app.route("/UserLogin", methods=['POST'])
-def UserLogin():
-    try:
-        email = request.json['email']
-        password = request.json['password']
-        cur = get_db().cursor()
-        checkUsername = cur.execute("Select email, password from user_profile where email=? Limit 1", (email,)).fetchall()
-        if not checkUsername:
-            response={}
-            response["error"] = "Username doesnt exists !!!!!"
-            return Response(json.dumps(response), status=406, mimetype='application/json')
-        savedPass = checkUsername[0][1]
-        if savedPass = password:
-            return Response(status = 200)
-        else:
-            response = {}
-            respose["error"] = "Incorrect Password"
-            return Response(json.dumps(response), status=406, mimetype='application/json')
-    except Exception as e:
-        print(e)
         return Response(status=400)
 
+    if request.method == 'GET':
+        #GET USER HERE
+        try:
+            email = request.json['email']
+            cur = get_db().cursor()
+            res = cur.execute("Select id, first_name, last_name, email, password, account_created, account_updated from user_info")
+            for row in res:
+                items = {}
+                items['id'] = str(row[0]) 
+                items['first_name'] = str(row[1])
+                items['last name'] = str(row[2])
+                items['email'] = str(row[3])
+                items['password'] = str(row[4])
+                items['account_created'] = str(row[5])
+                items['account_updated'] = str(row[6])
+                return Response(json.dumps(status), status=200, mimetype='application/json')
+            return Response(status=401)
+        except Exception as e:
+            print(e)
+            return Response(status=400)
+
+    if request.method == 'PUT':
+        #UPDATE USER
+        try:
+            #UPDATE PASSWORD
+            try:
+                email = request.json['email']
+                oldPassword = request.json['oldpassword']
+                newPassword = request.json['newpassword']
+                cur = get_db().cursor()
+                checkUsername = cur.execute("Select email, password from user_profile where email=? Limit 1", (email,)).fetchall()
+                if not checkUsername:
+                    response = {}
+                    response["error"] = "Username not found"
+                    return Response(json.dumps(response), status=400, mimetype='application/json')
+                savedPass == checkUsername[0][1]
+                if savedPass == oldPassword:
+                    cur.execute("UPDATE user_profile SET password =? where username=?", (newPassword, email,))
+                    get_db().commit()
+                    return Response(status=200)
+                else:
+                    response = {}
+                    response["error"] = "Incorrect Password !!!!!"
+                    return Response(json.dumps(response), status=400, mimetype='application/json')
+            execpt Exception as e:
+                get_db().rollback()
+                print(e)
+                return Response(status=400)
+
+            #UPDATE FIRST NAME
+            #UPDATE LAST NAME
+            try:
+                email = request.json['email']
+                first_name = request.json['first_name']
+                last_name = request.json['last_name']
+                cur = get_db().cursor()
+                checkUser = cur.execute("Select email from user_profile where email=? Limit 1", (email,)).fetchall()
+                if not checkUser:
+                    response = {}
+                    response["error"] = "User Not Found !!!!!"
+                    return Response(json.dumps(response), status=400, mimetype='application/json')
+
+                cur.execute("UPDATE user_profile SET first_name=?, last_name=? where email=?",(first_name, last_name, email))
+                get_db().commit()
+                return Response(status=200)
+            except Exception as e:
+                get_db().rollback()
+                print(e)
+                return Response(status=400)
+
+        except Exception as e:
+            get_db().rollback()
+            print(e)
+            return Response(status=401)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
