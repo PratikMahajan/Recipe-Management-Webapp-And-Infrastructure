3ef76d6a5f73302ff9482458d5be222dae5c75e5
pratikmahajan <pratik@mahajan.xyz>
2019-09-24 23:54:44 -0400

    refactor start

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from models import Base, User
+from models.user_data import Base, User
 from flask import Flask,Response, jsonify, request, abort,g
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
@@ -12,9 +12,11 @@ auth = HTTPBasicAuth()
 
 engine = create_engine('mysql+pymysql://user:user@localhost/user',echo=True)
 
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+def get_db():
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    return session
 
 app = Flask(__name__)
 
@@ -23,9 +25,9 @@ def verify_password(username_or_token, password):
     #Try to see if it's a token first
     user_id = User.verify_auth_token(username_or_token)
     if user_id:
-        user = session.query(User).filter_by(id = user_id).one()
+        user = get_db().query(User).filter_by(id = user_id).one()
     else:
-        user = session.query(User).filter_by(email_address = username_or_token).first()
+        user = get_db().query(User).filter_by(email_address = username_or_token).first()
         if not user or not user.verify_password(password):
             return False
     g.user = user
@@ -39,21 +41,22 @@ def get_auth_token():
 
 @app.route('/v1/user', methods = ['POST'])
 def new_user():
+    cursor=get_db()
     username = request.json.get('email_address')
     password = request.json.get('password')
     if username is None or password is None:
-        print("missing arguments")
-        abort(400) 
+        status={'ERROR':'Missing Arguments'}
+        return Response(json.dumps(status), status=400, mimetype='application/json')
         
     if session.query(User).filter_by(email_address = username).first() is not None:
-        print("existing user")
-        abort(400)
+        status = {'ERROR': 'User Error'}
+        return Response(json.dumps(status), status=400, mimetype='application/json')
 
     user = User(id=str(uuid.uuid4()),first_name =request.json.get('first_name'),last_name=request.json.get('last_name'),
                 email_address = username,account_created=str(datetime.now()),account_updated=str(datetime.now()))
     user.bcrypt_salt_hash(password)
-    session.add(user)
-    session.commit()
+    cursor.add(user)
+    cursor.commit()
     return jsonify({ 'id':user.id,'first_name':user.first_name,'last_name':user.last_name,'email_address':user.email_address,
                      'account_created': user.account_created,'account_updated':user.account_updated}), 201
 
@@ -67,9 +70,10 @@ def get_user():
 @app.route('/v1/user/self', methods = ['PUT'])
 @auth.login_required
 def update_user():
+    cursor=get_db()
     if ((request.json.get('id') is not  None) or  (request.json.get('email_address') is not None) or (request.json.get('account_created') is not None)
             or (request.json.get('account_updated') is not None)):
-        abort(400)
+        return Response(status=400, mimetype='application/json')
     else:
         if request.json.get('first_name') is not None:
             g.user.first_name=request.json.get('first_name')
@@ -78,7 +82,7 @@ def update_user():
         if request.json.get('password') is not None:
             g.user.bcrypt_salt_hash(request.json.get('password'))
         g.user.account_updated=str(datetime.now())
-        session.commit()
+        cursor.commit()
         return jsonify({}), 204
 
 @app.route('/health', methods=['GET', 'POST'])
