ea5ac1a355febd7685dcba108571c1d9629c7320
pratikmahajan <pratik@mahajan.xyz>
2019-09-25 00:23:08 -0400

    app.py according to coding conventions

--- webapp/app.py
+++ webapp/app.py
@@ -11,7 +11,11 @@ from config.envvar import *
 
 auth = HTTPBasicAuth()
 
-engine = create_engine('mysql+pymysql://user:user@localhost/user',echo=True)
+engine = create_engine('mysql+pymysql://'+db_config["DB_USER"]+':'+db_config["DB_PASSWORD"]+'@'+db_config["DB_HOST"]+'/'
+                       + db_config["DB_NAME"], echo=True)
+
+app = Flask(__name__)
+
 
 def get_db():
     Base.metadata.bind = engine
@@ -19,21 +23,20 @@ def get_db():
     session = DBSession()
     return session
 
-app = Flask(__name__)
-
 
 @auth.verify_password
 def verify_password(username_or_token, password):
-    #Try to see if it's a token first
+    # Try to see if it's a token first
     user_id = User.verify_auth_token(username_or_token)
     if user_id:
-        user = get_db().query(User).filter_by(id = user_id).one()
+        user = get_db().query(User).filter_by(id=user_id).one()
     else:
-        user = get_db().query(User).filter_by(email_address = username_or_token).first()
+        user = get_db().query(User).filter_by(email_address=username_or_token).first()
         if not user or not user.verify_password(password):
             return False
     g.user = user
     return True
+
 
 @app.route('/token')
 @auth.login_required
@@ -41,7 +44,8 @@ def get_auth_token():
     token = g.user.generate_auth_token()
     return jsonify({'token': token.decode('ascii')})
 
-@app.route('/v1/user', methods = ['POST'])
+
+@app.route('/v1/user', methods=['POST'])
 def new_user():
     cursor=get_db()
     username = request.json.get('email_address')
@@ -54,22 +58,25 @@ def new_user():
         status = {'ERROR': 'User Error'}
         return Response(json.dumps(status), status=400, mimetype='application/json')
 
-    user = User(id=str(uuid.uuid4()),first_name =request.json.get('first_name'),last_name=request.json.get('last_name'),
-                email_address = username,account_created=str(datetime.now()),account_updated=str(datetime.now()))
+    user = User(id=str(uuid.uuid4()), first_name=request.json.get('first_name'), last_name=request.json.get('last_name'),
+                email_address=username, account_created=str(datetime.now()), account_updated=str(datetime.now()))
     user.bcrypt_salt_hash(password)
     cursor.add(user)
     cursor.commit()
-    return jsonify({ 'id':user.id,'first_name':user.first_name,'last_name':user.last_name,'email_address':user.email_address,
-                     'account_created': user.account_created,'account_updated':user.account_updated}), 201
+    return jsonify({'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name,
+                    'email_address': user.email_address, 'account_created': user.account_created,
+                    'account_updated': user.account_updated}), 201
 
-@app.route('/v1/user/self', methods = ['GET'])
+
+@app.route('/v1/user/self', methods=['GET'])
 @auth.login_required
 def get_user():
-    return jsonify({'id':g.user.id,'first_name':g.user.first_name,'last_name':g.user.last_name,'email_address':g.user.email_address,
-                    'account_created': g.user.account_created,'account_updated':g.user.account_updated}), 200
+    return jsonify({'id': g.user.id, 'first_name': g.user.first_name, 'last_name': g.user.last_name,
+                    'email_address': g.user.email_address, 'account_created': g.user.account_created,
+                    'account_updated': g.user.account_updated}), 200
 
 
-@app.route('/v1/user/self', methods = ['PUT'])
+@app.route('/v1/user/self', methods=['PUT'])
 @auth.login_required
 def update_user():
     cursor=get_db()
@@ -83,13 +90,14 @@ def update_user():
             g.user.last_name=request.json.get('last_name')
         if request.json.get('password') is not None:
             g.user.bcrypt_salt_hash(request.json.get('password'))
-        g.user.account_updated=str(datetime.now())
+        g.user.account_updated = str(datetime.now())
         cursor.commit()
         return jsonify({}), 204
 
+
 @app.route('/health', methods=['GET', 'POST'])
 @disable_logging
-def health() -> Response:
+def health_probe() -> Response:
     status = dict()
     status["ok"] = True
     return Response(json.dumps(status), status=200, mimetype='application/json')
--- webapp/models/user_data.py
+++ webapp/models/user_data.py
@@ -3,14 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 import bcrypt
-import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+import random
+import string
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from config.envvar import *
+
 
 Base = declarative_base()
 secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
 
 class User(Base):
-    __tablename__ = 'user_info'
+    __tablename__ = db_config["DB_NAME"]
     id = Column(String(128), primary_key=True)
     first_name = Column(String(32))
     last_name = Column(String(32))
@@ -19,31 +22,31 @@ class User(Base):
     account_created = Column(String(64))
     account_updated = Column(String(64))
 
-    def bcrypt_salt_hash(self,password):
-        salt=bcrypt.gensalt(rounds=16)
-        self.password=bcrypt.hashpw(password.encode('utf-8'),salt)
+    def bcrypt_salt_hash(self, password):
+        salt = bcrypt.gensalt(rounds=16)
+        self.password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
-    def verify_password(self,password):
-        return bcrypt.checkpw(password.encode('utf-8'),self.password.encode('utf-8'))
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
 
-    def gen_auth_token(self,exp=600):
-        s=Serializer(secret_key,expires_in=exp)
-        return s.dumps({'id':self.id})
+    def gen_auth_token(self, exp=600):
+        s=Serializer(secret_key, expires_in=exp)
+        return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
-        s=Serializer(secret_key)
+        s = Serializer(secret_key)
         try:
             data=s.loads(token)
         except SignatureExpired:
             return None
         except BadSignature:
             return None
-        user_id=data['id']
+        user_id = data['id']
         return user_id
 
-engine = create_engine('mysql+pymysql://user:user@localhost/user',echo=True)
 
-Base.metadata.create_all(engine)  
+engine = create_engine('mysql+pymysql://'+db_config["DB_USER"]+':'+db_config["DB_PASSWORD"]+'@'+db_config["DB_IP"]+'/'
+                       + db_config["DB_NAME"], echo=True)
 
-
+Base.metadata.create_all(engine)
