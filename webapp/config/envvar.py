336ce6e46573cd25290d82cec92310813d97a814
pratikmahajan <pratik@mahajan.xyz>
2019-09-25 00:17:08 -0400

    code writing conventions

--- webapp/config/envvar.py
+++ webapp/config/envvar.py
@@ -4,4 +4,4 @@ db_config = dict()
 db_config["DB_USER"] = os.environ['DB_USER']
 db_config["DB_PASSWORD"] = os.environ['DB_PASSWORD']
 db_config["DB_NAME"] = os.environ['DATABASE_NAME']
-
+db_config["DB_IP"] = os.environ["DB_IP"]
