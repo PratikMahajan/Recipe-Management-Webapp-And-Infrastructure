80856b3fd783e3c5fbaabc68a0d61f4825159c97
AkashJ-487 <jagtap.ak@northeastern.edu>
2019-10-01 23:08:01 -0400

    Making steps as a separate model

---
+++ webapp/models/steps.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column,Integer,String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from config.envvar import *
+
+class Steps(Base):
+    __tablename__ = "steps"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    recipe_id = Column(String(128))
+    position = Column(Integer)
+    items = Column(String(256))
+
+engine = create_engine('mysql+pymysql://'+db_config["DB_USER"]+':'+db_config["DB_PASSWORD"]+'@'+db_config["DB_HOST"]+'/'
+                       + db_config["DB_NAME"])
+
+Base.metadata.create_all(engine)
+
