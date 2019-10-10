df36d862db5edf0a41bd51b3f002d8bffc9de492
pratikmahajan <pratik@mahajan.xyz>
2019-10-09 20:35:28 -0400

    cloud formation setup complete

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-stack.sh
@@ -7,50 +7,42 @@ key="$1"
 case $key in
     -d|--dev)
     export AWS_PROFILE=dev
-    shift # past argument
-    shift # past value
     ;;
     -p|--prod)
     export AWS_PROFILE=prod
-    shift # past argument
-    shift # past value
     ;;
 esac
 
-#f () {
-#    errcode=$? # Error Code
-#    echo "ERROR $errcode"
-#    echo "While executing: "
-#    echo "$BASH_COMMAND"
-#    echo "on line ${BASH_LINENO[0]}"
-#    echo "Exiting..."
-#    exit $errcode
-#}
-
-#trap f ERR
-
-if [-Z "$2"]; then 
-	echo "Please enter the stack name"
+## Checking whether the stack-name is passed as an arguement
+if [ -z "$2" ]; then
+	echo "Please enter the stack name";
+	exit 1;
 else
 	echo "VPC stack getting created...."
-
-stack_Name= $2
-echo "Stack name is $stack_Name-csye6225-vpc"
-
-createStack=$(aws cloudformation create-stack --stack-name $stack_Name --template-body file://csye6225-cf-networking.json --parameters ParameterKey=stack_Name,ParameterValue=$stackName)
-
-if [$? -eq "0"]; then
-	completeStack=$(aws cloudformation wait stack-create-complete --stack-name $stack_Name)
-	if [$? -eq "0"]; then
-		echo "Stack is successfully created"
-	else
-		echo "Unsuccessful in creating stack"
-	fi
-else
-	echo "Unable to create CloudFormation"
-	echo $createStack
- fi
 fi
 
+echo "Creating cloud stack with name: $2"
+
+stackList=$(aws cloudformation list-stacks --query 'StackSummaries[?StackStatus != `DELETE_COMPLETE`].{StackName:StackName}')
 
 
+if [  `echo $stackList | grep -w -c $2 ` -gt 0 ]
+then
+  echo "Stack with name: $2  exists"
+  echo "Stack creation failed"
+  echo "Exiting.."
+  exit 1
+fi
+
+#Replacing the STACK_NAME passed by the user in the csye6225-cf-networking-parameters.json
+sed -i "s/REPLACE_STACK_NAME/$2/g" csye6225-cf-networking-parameters.json
+
+##Creating Stack
+#echo "Creating Cloud Stack $1"
+response=$(aws cloudformation create-stack --stack-name "$2" --template-body file://csye6225-cf-networking.json --parameters file://csye6225-cf-networking-parameters.json)
+echo "Waiting for Stack $2 to be created"
+echo "$response"
+
+aws cloudformation wait stack-create-complete --stack-name $2
+echo "stack $2 created successfully"
+sed -i "s/$2/REPLACE_STACK_NAME/g" csye6225-cf-networking-parameters.json
