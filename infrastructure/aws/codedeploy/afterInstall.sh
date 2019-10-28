7fa74b1cad2da3c8d01c2c182704f1e807271a1c
pratikmahajan <pratik@mahajan.xyz>
2019-10-28 18:55:41 -0400

    added permissions for accessing webapp folder

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+sudo chown -R centos:centos /home/centos/webapp/
 pip3 install -r requirements.txt --user
 
-{
-  export DB_USER=$DB_USER
-  export DB_PASSWORD=$DB_PASSWORD
-  export DATABASE_NAME=$DATABASE_NAME
-  export DB_HOST=$DB_HOST
-} >> /etc/environment
+
+#{
+#  export DB_USER=$DB_USER
+#  export DB_PASSWORD=$DB_PASSWORD
+#  export DATABASE_NAME=$DATABASE_NAME
+#  export DB_HOST=$DB_HOST
+#} >> /etc/environment
 
 #make run
 
