da17f05b1593a9ab074b375f59ff62eb4806c5fa
pratikmahajan <pratik@mahajan.xyz>
2019-10-29 03:59:44 -0400

    added log creation to afterninstall.sh

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -6,6 +6,7 @@ pip3 install -r /home/centos/webapp/scripts/requirements.txt --user
 
 sudo mkdir /home/centos/logs
 sudo touch /home/centos/logs/gunicorn.log
+sudo tail -n 0 -f /home/centos/logs/gunicorn*.log &
 sudo chown -R centos:centos /home/centos/logs/
 
 sudo mv /home/centos/webapp/gunicorn.service /etc/systemd/system/
