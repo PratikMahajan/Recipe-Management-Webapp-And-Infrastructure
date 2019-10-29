b1ff8996b98056cd5db756802acbdad83e22aa51
pratikmahajan <pratik@mahajan.xyz>
2019-10-29 03:57:53 -0400

    added detailed afterinstall script

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -4,6 +4,17 @@
 sudo chown -R centos:centos /home/centos/webapp/
 pip3 install -r /home/centos/webapp/scripts/requirements.txt --user
 
+sudo mkdir /home/centos/logs
+sudo touch /home/centos/logs/gunicorn.log
+sudo chown -R centos:centos /home/centos/logs/
+
+sudo mv /home/centos/webapp/gunicorn.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl status gunicorn
+
+
+
 
 #read -d '' data <<EOF
 #DB_USER=$DB_USER
