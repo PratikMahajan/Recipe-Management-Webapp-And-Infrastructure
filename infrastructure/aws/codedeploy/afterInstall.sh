1022a1175bd6250d29341c0f2926a84469376ad8
pratikmahajan <pratik@mahajan.xyz>
2019-10-30 02:24:19 -0400

    removed tail command

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -2,7 +2,7 @@
 
 
 sudo chown -R centos:centos /home/centos/webapp/
-pip3 install -r /home/centos/webapp/scripts/requirements.txt --user 2>&1 &
+pip3 install -r /home/centos/webapp/scripts/requirements.txt --user 2>&1
 
 dir_name=/home/centos/logs
 if [ -d "$dir_name" ]; then
@@ -11,18 +11,17 @@ if [ -d "$dir_name" ]; then
 fi
 sudo mkdir -p /home/centos/logs
 sudo touch /home/centos/logs/gunicorn.log
-sudo tail -n 0 -f /home/centos/logs/gunicorn*.log &
 sudo chown -R centos:centos /home/centos/logs/
 
 sudo mv /home/centos/webapp/gunicorn.service /etc/systemd/system/
-sudo systemctl daemon-reload >/dev/null 2>&1 &
-sudo systemctl start gunicorn >/dev/null 2>&1 &
+sudo systemctl daemon-reload >/dev/null 2>&1
+sudo systemctl start gunicorn >/dev/null 2>&1
 #sudo systemctl status gunicorn
 
 sudo cp /home/centos/webapp/Caddyfile /etc/caddy/
 sudo mv /home/centos/webapp/caddy.service /etc/systemd/system/
-sudo systemctl daemon-reload >/dev/null 2>&1 &
-sudo systemctl start caddy >/dev/null 2>&1 &
+sudo systemctl daemon-reload >/dev/null 2>&1
+sudo systemctl start caddy >/dev/null 2>&1
 #sudo systemctl status caddy
 echo "---------After Install Script Execution Completed------------"
 exit 0
